c2e12db5ac38da59a6984ab7c64c4807c4665a56
Chris Cartwright <chris_cart7@hotmail.com>
2015-05-24 13:38:49 -0700

    contact page

--- templates/contact.php
+++ templates/contact.php
@@ -2,24 +2,30 @@
 
 <form id="form2" action="send.php" method="post">
     <fieldset><legend>Contact form</legend>
-        <p class="first">
-            <label for="firstName">First Name</label>
-            <input type="text" name="firstName" id="firstName" placeholder="First Name" required maxlength="25" />
-        </p>
-        <p>
-            <label for="lastName">Last Name</label>
-            <input type="text" name="lastName" id="lastName" placeholder="Last Name" required maxlength="25" />
-        </p>
-        <p class="third">
-            <label for="email">Email</label>
-            <input type="text" name="email" id="email" placeholder="Email" required maxlength="40" />
-        </p>
-        <p>
-            <label id="Lblmessage" for="message">Message</label>
-            <textarea name="message" id="message" placeholder="Message" required maxlength="10000" style="height: 20em;"></textarea>
-        </p>
+        <div class="form-inline">
+            <p>
+                <label for="firstName">First Name</label>
+                <input type="text" name="firstName" id="firstName" placeholder="First Name" required maxlength="25" />
+            </p>
+            <p>
+                <label for="lastName">Last Name</label>
+                <input type="text" name="lastName" id="lastName" placeholder="Last Name" required maxlength="25" />
+            </p>
+            <p>
+                <label for="email">Email</label>
+                <input type="text" name="email" id="email" placeholder="Email" required maxlength="40" />
+            </p>
+        </div>
+        <div class="form-inline">
+            <p>
+                <label id="Lblmessage" for="message">Message</label>
+                <textarea name="message" id="message" placeholder="Message" required maxlength="10000" ></textarea>
 
-        <p class="submit"><input type="submit" name="submit" value="Send" /></p>
+            </p>
+            <p>
+                <input type="submit" name="submit" value="Send" />
+            </p>
+        </div>
 
     </fieldset>
 </form>
